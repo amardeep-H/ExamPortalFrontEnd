1d361e134d7770162541c487fab808bb06f21549
Amardeep Honmane <rajdeepsonar@gmail.com>
2025-05-02 22:02:45 +0530

    Update helper.ts
    testing for local env

--- examServerFrontEnd/examServerFrontEnd/src/app/services/helper.ts
+++ examServerFrontEnd/examServerFrontEnd/src/app/services/helper.ts
@@ -1,4 +1,6 @@
-let baseUrl = 'http://localhost:8282';
+
+let baseUrl = 'http://localhost:8081';
+// let baseUrl = 'http://localhost:8282';
 // let baseUrl = 'http://examserver.online:8080'
 // let baseUrl = 'https://api.examserver.online';
 export default baseUrl
